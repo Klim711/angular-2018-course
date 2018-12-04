868156e25e76485500366f73590e1f0b5b74a5c4
Klim <klim1996@gmail.com>
2018-12-04 22:46:14 +0300

    Remove unneeded 'main' component

--- src/app/listing/listing.module.ts
+++ src/app/listing/listing.module.ts
@@ -1,8 +1,6 @@
-import { CoreModule } from './../core/core.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListingPageComponent } from './listing-page/listing-page.component';
-import { MainComponent } from './main/main.component';
 import { CoursesToolbarComponent } from './courses-toolbar/courses-toolbar.component';
 import { CoursesListComponent } from './courses-list/courses-list.component';
 import { CoursesListItemComponent } from './courses-list-item/courses-list-item.component';
@@ -10,14 +8,12 @@ import { CoursesListItemComponent } from './courses-list-item/courses-list-item.
 @NgModule({
   declarations: [
     ListingPageComponent,
-    MainComponent,
     CoursesToolbarComponent,
     CoursesListComponent,
     CoursesListItemComponent
   ],
   imports: [
-    CommonModule,
-    CoreModule
+    CommonModule
   ],
   exports: [
     ListingPageComponent,
