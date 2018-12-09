bf019cd564e9b0abcd335ad9b181f6fe5e10001b
Klim <klim1996@gmail.com>
2018-12-09 20:34:40 +0300

    Pass list of courses from parent component

--- src/app/listing/courses-list/courses-list.component.ts
+++ src/app/listing/courses-list/courses-list.component.ts
@@ -1,5 +1,5 @@
 import { CoursesListItem } from './../courses-list-item.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-courses-list',
@@ -7,29 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-list.component.css']
 })
 export class CoursesListComponent implements OnInit {
-  public courses: CoursesListItem[] = [
-    {
-      id: 1,
-      title: 'A',
-      create_date: '1/1/2011',
-      duration: 123,
-      description: 'AAA',
-    },
-    {
-      id: 2,
-      title: 'B',
-      create_date: '1/1/2011',
-      duration: 123,
-      description: 'BBB',
-    },
-    {
-      id: 3,
-      title: 'C',
-      create_date: '1/1/2011',
-      duration: 123,
-      description: 'CCC',
-    },
-  ];
+  @Input() public courses: CoursesListItem[];
 
   constructor() { }
 
--- src/app/listing/listing-page/listing-page.component.ts
+++ src/app/listing/listing-page/listing-page.component.ts
@@ -1,3 +1,4 @@
+import { CoursesListItem } from './../courses-list-item.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing-page.component.css']
 })
 export class ListingPageComponent implements OnInit {
+  public courses: CoursesListItem[];
 
   constructor() { }
 
   ngOnInit() {
+    this.courses = [
+      {
+        id: 1,
+        title: 'A',
+        create_date: '1/1/2011',
+        duration: 123,
+        description: 'AAA',
+      },
+      {
+        id: 2,
+        title: 'B',
+        create_date: '1/1/2011',
+        duration: 123,
+        description: 'BBB',
+      },
+      {
+        id: 3,
+        title: 'C',
+        create_date: '1/1/2011',
+        duration: 123,
+        description: 'CCC',
+      },
+      {
+        id: 4,
+        title: 'D',
+        create_date: '1/1/2011',
+        duration: 123,
+        description: 'DDD',
+      },
+      {
+        id: 5,
+        title: 'E',
+        create_date: '1/1/2011',
+        duration: 123,
+        description: 'EEE',
+      },
+      {
+        id: 6,
+        title: 'F',
+        create_date: '1/1/2011',
+        duration: 123,
+        description: 'FFF',
+      },
+    ];
   }
 
 }
