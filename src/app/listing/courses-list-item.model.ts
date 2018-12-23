8534226d032a63f5623c554b11b386f2fdacb737
Klim <klim1996@gmail.com>
2018-12-23 15:41:35 +0300

    Display create_date using date pipe

--- src/app/listing/courses-list-item.model.ts
+++ src/app/listing/courses-list-item.model.ts
@@ -1,7 +1,7 @@
 export interface CoursesListItem {
   id: number;
   title: string;
-  create_date: string;
+  create_date: Date;
   duration: number;
   description: string;
   rating: number | null;
--- src/app/listing/listing-page/listing-page.component.ts
+++ src/app/listing/listing-page/listing-page.component.ts
@@ -16,7 +16,7 @@ export class ListingPageComponent implements OnInit {
       {
         id: 1,
         title: 'a',
-        create_date: '12/20/2018',
+        create_date: new Date('12/20/2018'),
         duration: 123,
         description: 'AAA',
         rating: 9,
@@ -24,7 +24,7 @@ export class ListingPageComponent implements OnInit {
       {
         id: 2,
         title: 'b',
-        create_date: '1/1/2019',
+        create_date: new Date('1/1/2019'),
         duration: 33,
         description: 'BBB',
         rating: 7,
@@ -32,7 +32,7 @@ export class ListingPageComponent implements OnInit {
       {
         id: 3,
         title: 'c',
-        create_date: '1/1/2020',
+        create_date: new Date('1/1/2020'),
         duration: 120,
         description: 'CCC',
         rating: 4,
@@ -40,7 +40,7 @@ export class ListingPageComponent implements OnInit {
       {
         id: 4,
         title: 'D',
-        create_date: '1/1/2011',
+        create_date: new Date('1/1/2011'),
         duration: 25,
         description: 'DDD',
         rating: null,
@@ -48,7 +48,7 @@ export class ListingPageComponent implements OnInit {
       {
         id: 5,
         title: 'E',
-        create_date: '1/1/2011',
+        create_date: new Date('1/1/2011'),
         duration: 123,
         description: 'EEE',
         rating: 60,
@@ -56,7 +56,7 @@ export class ListingPageComponent implements OnInit {
       {
         id: 6,
         title: 'F',
-        create_date: '1/1/2011',
+        create_date: new Date('1/1/2011'),
         duration: 59,
         description: 'FFF',
         rating: 6,
