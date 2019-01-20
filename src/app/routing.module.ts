7117dd9d3fc836a326fbed696884efb45db9cb4b
Klim Shuplenkou <kshuplenkou@google.com>
2019-01-20 17:00:21 +0300

    Add simple login-page

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,9 +2,11 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ListingPageComponent } from './listing/listing-page/listing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes  } from '@angular/router';
+import { LoginPageComponent } from './login/login-page/login-page.component';
 
 const ROUTES: Routes = [
   { path: 'listing', component: ListingPageComponent },
+  { path: 'login', component: LoginPageComponent },
   { path: '', redirectTo: '/listing', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
