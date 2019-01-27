2bfe77f3bfab7bc728692ae70225bb31ee6d5b99
Klim Shuplenkou <kshuplenkou@google.com>
2019-01-27 18:05:21 +0300

    Add displaying of course-duration near input

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { CoursePageComponent } from './course-page/course-page.component';
 import { CourseFormComponent } from './course-form/course-form.component';
 import { FormsModule }   from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [CoursePageComponent, CourseFormComponent],
+  declarations: [
+    CoursePageComponent,
+    CourseFormComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
+    SharedModule,
   ],
   exports: [CoursePageComponent],
 })
