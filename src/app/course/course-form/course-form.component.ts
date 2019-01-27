4476bf6a219455f22bcc3fc5d1d418dfd39ec88e
Klim Shuplenkou <kshuplenkou@google.com>
2019-01-27 17:42:39 +0300

    Add bindings for course-form

--- src/app/course/course-form/course-form.component.ts
+++ src/app/course/course-form/course-form.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-form.component.css']
 })
 export class CourseFormComponent implements OnInit {
+  public title: String = '';
+  public description: String = '';
+  public date: String = '';
+  public duration: String = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  save() {
+    alert(`
+      Following course was saved:
+      Title: ${this.title}
+      Description: ${this.description}
+      Date: ${this.date}
+      Duration: ${this.duration}
+    `);
+  }
+
+  cancel() {
+    this.title = '';
+    this.description = '';
+    this.date = '';
+    this.duration = '';
+  }
 }
--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursePageComponent } from './course-page/course-page.component';
 import { CourseFormComponent } from './course-form/course-form.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [CoursePageComponent, CourseFormComponent],
   imports: [
     CommonModule,
+    FormsModule,
   ],
   exports: [CoursePageComponent],
 })
