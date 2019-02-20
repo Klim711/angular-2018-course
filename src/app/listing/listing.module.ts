43969cd949affb01425d86d4f9a4b5ca844c7d3f
Klim Shuplenkou <kshuplenkou@google.com>
2019-02-20 20:37:12 +0300

    Remove redundant courses-list component

--- src/app/listing/listing.module.ts
+++ src/app/listing/listing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListingPageComponent } from './listing-page/listing-page.component';
 import { CoursesToolbarComponent } from './courses-toolbar/courses-toolbar.component';
-import { CoursesListComponent } from './courses-list/courses-list.component';
 import { CoursesListItemComponent } from './courses-list-item/courses-list-item.component';
 import { SearchComponent } from './search/search.component';
 import { AddCourseComponent } from './add-course/add-course.component';
@@ -15,7 +14,6 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     ListingPageComponent,
     CoursesToolbarComponent,
-    CoursesListComponent,
     CoursesListItemComponent,
     SearchComponent,
     AddCourseComponent,
