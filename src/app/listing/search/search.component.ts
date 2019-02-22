8fe963c536c61243b4ace8e244e02127d03fe859
Klim Shuplenkou <kshuplenkou@google.com>
2019-02-22 23:46:08 +0300

    Add obsearvable on search input

--- src/app/listing/search/search.component.ts
+++ src/app/listing/search/search.component.ts
@@ -1,25 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
+import { fromEvent } from 'rxjs';
+import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements AfterViewInit {
+  @ViewChild('searchInput') search: ElementRef;
   public value: string = '';
 
   constructor(private coursesService:CoursesService) {}
 
-  ngOnInit() {
-  }
-
-  onKey(event: KeyboardEvent) {
-    const searchValue = (<HTMLInputElement>event.target).value;
-    this.value = searchValue;
-  }
-
-  search() {
-    this.coursesService.setSearchValue(this.value);
+  ngAfterViewInit() {
+    fromEvent(this.search.nativeElement, 'input').pipe(
+      map(({target}:{target: HTMLInputElement}) => target.value),
+      // filter(text => text.length > 2),
+      debounceTime(50),
+      distinctUntilChanged()
+    ).subscribe((value) => {
+      this.coursesService.setSearchValue(value);
+    });
   }
 }
