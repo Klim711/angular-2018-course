72245e1305a4f095cb8c34d2656cec3c7a5c81d2
Klim Shuplenkou <kshuplenkou@google.com>
2019-02-27 22:30:21 +0300

    Return Observable in auth guard

--- src/app/core/services/auth.guard.ts
+++ src/app/core/services/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -22,9 +22,9 @@ export class AuthGuard implements CanActivate {
           return: state.url
         }
       });
-      return false;
+      return of(false);
     } else {
-      return true;
+      return of(true);
     }
   }
 }
