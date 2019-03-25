bf46dd6947741660ffa375fdb0be35657caccac5
Klim Shuplenkou <kshuplenkou@google.com>
2019-03-25 23:23:06 +0300

    Handle onTouch for course-date component

--- src/app/course/course-date/course-date.component.ts
+++ src/app/course/course-date/course-date.component.ts
@@ -17,19 +17,24 @@ export class CourseDateComponent implements ControlValueAccessor {
   writeValue(value: any) {
     if (value !== undefined) {
       this.value = value;
-      this.propagateChange(this.value);
+      this.onChange(this.value);
     }
   }
 
-  propagateChange = (_: any) => {};
+  onChange = (_: any) => {};
 
   registerOnChange(fn) {
-    this.propagateChange = fn;
+    this.onChange = fn;
   }
 
-  onChange() {
-    this.propagateChange(this.value);
+  onTouched = () => {};
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
   }
 
-  registerOnTouched() {}
+  onDateChange() {
+    this.onChange(this.value);
+    this.onTouched();
+  }
 }
